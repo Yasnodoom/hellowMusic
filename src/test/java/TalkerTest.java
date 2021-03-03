155ea550c4aba804ea394e85146e91704ec1a929
KlimakovAE <KlimakovAE@it.mos.ru>
2021-03-03 19:44:33 +0300

    headless

--- src/test/java/TalkerTest.java
+++ src/test/java/TalkerTest.java
@@ -14,6 +14,7 @@ public class TalkerTest {
     @BeforeClass
     public static void setup() {
         Configuration.browser = "firefox";
+        Configuration.headless = false;
     }
 
     @Test
